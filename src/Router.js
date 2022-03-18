29dbbe01adf8b0fe60bf33598202cfc51133cd31
Dixit Parmar <Dparmar006@users.noreply.github.com>
2022-03-18 12:04:00 +0530

    Pushed to git

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Register from './pages/Auth/Register'
 import Dashboard from './pages/Dashboard'
 import Medicines from './pages/Medicines'
 import Stores from './pages/Stores'
+import RegisterStores from './pages/Stores/RegisterStores'
 const Router = () => {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path='/' element={<Dashboard />} />
         <Route path='/medicines' element={<Medicines />} />
         <Route path='/stores' element={<Stores />} />
+        <Route path='/stores/register-store' element={<RegisterStores />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
--- src/pages/Stores/RegisterStores.js
+++ src/pages/Stores/RegisterStores.js
@@ -1,9 +1,96 @@
-import { Breadcrumb } from 'antd'
 import React from 'react'
+import { Form, Input, InputNumber, Button, Row, Col } from 'antd'
 import BasicLayout from '../../Layout/BasicLayout'
 
 const RegisterStores = () => {
-  return <BasicLayout> </BasicLayout>
+  const onFinish = values => {
+    console.log(values)
+  }
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not a valid email!',
+      number: '${label} is not a valid number!'
+    },
+    number: {
+      range: '${label} must be between ${min} and ${max}'
+    }
+  }
+
+  return (
+    <BasicLayout>
+      <Form
+        name='nest-messages'
+        onFinish={onFinish}
+        validateMessages={validateMessages}
+      >
+        <Row gutter={24}>
+          <Col xl={6} md={12} sm={24}>
+            <Form.Item
+              name={['name']}
+              label='Name'
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xl={6} md={12} sm={24}>
+            <Form.Item
+              name={['user', 'name']}
+              label='City'
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item
+          name={['user', 'email']}
+          label='Email'
+          rules={[
+            {
+              type: 'email'
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'age']}
+          label='Age'
+          rules={[
+            {
+              type: 'number',
+              min: 0,
+              max: 99
+            }
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name={['user', 'website']} label='Website'>
+          <Input />
+        </Form.Item>
+        <Form.Item name={['user', 'introduction']} label='Introduction'>
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8 }}>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </BasicLayout>
+  )
 }
 
 export default RegisterStores
