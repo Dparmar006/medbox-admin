995fc1f95185a9c100600135ce68693fe08d4dfb
Dixit Parmar <Dparmar006@users.noreply.github.com>
2022-05-21 01:43:27 +0530

    Its achivable

--- src/pages/SystemPages/PageNotFound.js
+++ src/pages/SystemPages/PageNotFound.js
@@ -1,7 +1,20 @@
+import { Result } from 'antd'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>
+  return (
+    <Result
+      status='404'
+      title='404'
+      subTitle='Sorry, the page you visited does not exist.'
+      extra={
+        <Link to='/' type='primary'>
+          Back Home
+        </Link>
+      }
+    />
+  )
 }
 
 export default PageNotFound
